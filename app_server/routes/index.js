2eed4ee71b098b0caf2945f0d04f70e20194f9e8
zhangshiqiu <zhangshiqiu1993@126.com>
2016-12-17 22:41:31 -0800

    map routes to controllers

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var ctrlLocation = require('../controllers/locations');
+var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
 
-var ctrlMain = require('../controllers/main');
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
