23866afbf11eda94ceca3d4898e5b554144fa493
zhangshiqiu <zhangshiqiu1993@126.com>
2016-12-22 19:45:04 -0800

    adding review page controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -10,5 +10,5 @@ module.exports.locationInfo = function(req, res){
 
 // Get 'Add review' page
 module.exports.addReview = function(req, res) {
-    res.render('index', {title: 'Add Review'});
+    res.render('location-review-form', {title: 'Add Review'});
 };
