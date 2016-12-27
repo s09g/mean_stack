95c04d30d9645ab3a58d2a2e52e73bfb40652c00
zhangshiqiu <zhangshiqiu1993@126.com>
2016-12-27 11:31:57 -0800

    connect to db

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,20 @@
 var mongoose = require('mongoose');
 
+var openingTimeSchema = new mongoose.Schema({
+    days: {type: String, required: true},
+    opening: String,
+    closing: String,
+    closed: {type: Boolean, required: true}
+});
+
+var locationSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    address: String,
+    rating: {type: Number, "default": 0, min: 0, max: 5},
+    facilities: [String],
+    coords: {type: [Number], index: '2dsphere'},
+    openingTimes: [openingTimeSchema]
+});
 
 // Get 'home' page
 module.exports.homelist = function(req, res){
