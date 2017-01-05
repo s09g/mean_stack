f7b0188b56493cf419eaab65014431f4bf995145
zhangshiqiu <zhangshiqiu1993@126.com>
2017-01-04 23:50:13 -0800

    rollback to last version

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -35,25 +35,25 @@ module.exports.homelist = function(req, res) {
             strapline: 'Find places to work with wifi near you!'
         },
         sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-        // locations: [{
-        //     name: 'Starcups',
-        //     address: '125 High Street, Reading, RG6 1PS',
-        //     rating: 3,
-        //     facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-        //     distance: '100m'
-        // }, {
-        //     name: 'Cafe Hero',
-        //     address: '125 High Street, Reading, RG6 1PS',
-        //     rating: 4,
-        //     facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-        //     distance: '200m'
-        // }, {
-        //     name: 'Burger Queen',
-        //     address: '125 High Street, Reading, RG6 1PS',
-        //     rating: 2,
-        //     facilities: ['Food', 'Premium wifi'],
-        //     distance: '250m'
-        // }]
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        }, {
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '200m'
+        }, {
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '250m'
+        }]
     });
 };
 
