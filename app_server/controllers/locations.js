1143269278b2bafabe6e4d146cd54da9d0fb0a05
zhangshiqiu <zhangshiqiu1993@126.com>
2017-01-06 23:30:02 -0800

    add review page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,31 +1,3 @@
-var mongoose = require('mongoose');
-
-var reviewSchema = new mongoose.Schema({
-    author: String,
-    rating: {type: Number, required: true, min: 0, max: 5},
-    reviewText: String,
-    createdOn: {type: Date, "dafault": Date.now}
-});
-
-var openingTimeSchema = new mongoose.Schema({
-    days: {type: String, required: true},
-    opening: String,
-    closing: String,
-    closed: {type: Boolean, required: true}
-});
-
-var locationSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    address: String,
-    rating: {type: Number, "default": 0, min: 0, max: 5},
-    facilities: [String],
-    coords: {type: [Number], index: '2dsphere'},
-    openingTimes: [openingTimeSchema],
-    reviews: [reviewSchema]
-});
-
-
-
 /* GET 'home' page */
 module.exports.homelist = function(req, res) {
     res.render('locations-list', {
@@ -106,7 +78,12 @@ module.exports.locationInfo = function(req, res) {
     });
 };
 
-// Get 'Add review' page
+/* GET 'Add review' page */
 module.exports.addReview = function(req, res) {
-    res.render('location-review-form', {title: 'Add Review'});
+    res.render('location-review-form', {
+        title: 'Review Starcups on Loc8r',
+        pageHeader: {
+            title: 'Review Starcups'
+        }
+    });
 };
